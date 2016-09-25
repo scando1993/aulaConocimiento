9f25eb4f155a2113ff4fdb4c35af241a4a7b3405
henryxavier10 <hlasso@espol.edu.ec>
2016-09-25 16:41:56 -0500

    resolucion de conflictos de login con home

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,7 +17,14 @@ Route::get('/', function () {
     return view('auth/login');
 });
 
-Route::post('loginme', 'loginController@login');
+
+Route::get('home','HomeController@index');
+
+
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', ['as' =>'auth/login', 'uses' => 'Auth\AuthController@postLogin']);
+Route::get('auth/logout', ['as' => 'auth/logout', 'uses' => 'Auth\AuthController@getLogout']);
+
 
 Route::get('listar', 'TallerController@listar');
 
@@ -30,3 +37,5 @@ Route::resource('taller','TallerController');
 Route::resource('recurso','RecursoController');
 
 Route::get('pdf', 'PdfController@invoice');
+
+
