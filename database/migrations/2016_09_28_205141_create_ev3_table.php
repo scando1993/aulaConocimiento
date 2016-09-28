b098c57aa486951011d083ad83548ecd2152d6ef
henryxavier10 <hlasso@espol.edu.ec>
2016-09-28 18:02:31 -0500

    creacion de tabla ev3

---
+++ database/migrations/2016_09_28_205141_create_ev3_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEv3Table extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('ev3', function ($table) {
+            $table->increments('id');
+            $table->integer('id_padre');
+            $table->string('titulo');
+            $table->string('descripcion');
+            $table->string('fuente');
+            $table->boolean('visible');
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('ev3');
+    }
+}
