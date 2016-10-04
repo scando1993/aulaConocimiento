ba92449b03c2ce0f0a2c0a53a218f61e0bc5152a
henryxavier10 <hlasso@espol.edu.ec>
2016-10-04 13:53:27 -0500

    resolviendo conflictos de ev3

--- resources/views/ev3.blade.php
+++ resources/views/ev3.blade.php
@@ -11,18 +11,11 @@
 			<div class="col-md-10 col-md-offset-1">
 				<div class="panel panel-default">
 					@foreach ($nombre as $ev3)
-    					{{$ev3->ruta}}
+    					@if ($ev3 == end($nombre))
+    						<embed src="/aulaConocimiento/resources/docs_blocks/Accion/{{$ev3->ruta}}" type="application/pdf" width="800" height="700"></embed>
+    					@endif
+
     				@endforeach
-
-
-					@if ($nombre=='motormediano')
-						<embed src="/aulaConocimiento/resources/docs_blocks/Accion/Bloque Motor mediano.pdf" type="application/pdf" width="800" height="700"></embed>	
-					@elseif($nombre=='motorgrande')
-						<embed src="/aulaConocimiento/resources/docs_blocks/Accion/Bloque Motor grande.pdf" type="application/pdf" width="800" height="700"></embed>		
-					@elseif($nombre=='moverdireccion')
-						<embed src="/aulaConocimiento/resources/docs_blocks/Accion/Bloque Mover la dirección.pdf" type="application/pdf" width="800" height="700"></embed>		
-					
-					@endif
 				</div>
 			</div>
 		</div>
