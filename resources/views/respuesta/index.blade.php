48417e20fc6379c67ee90fb652d61e1ef5ebe913
PCSERVER\thiago <santiagojr_2006@hotmail.com>
2016-10-13 00:58:07 -0500

    Corrección a la Vista Respuesta

--- resources/views/respuesta/index.blade.php
+++ resources/views/respuesta/index.blade.php
@@ -10,7 +10,9 @@
         <div class="col-lg-12 margin-tb">
             
             <div class="pull-right">
-                <a class="btn btn-success" href="{{ route('respuesta.create') }}"> Crear nueva respuesta</a>
+                <a class="btn btn-success" data-toggle="modal" data-target="#myModal">
+                  <span class="glyphicon glyphicon-plus"></span> Crear respuesta
+                 </a>
             </div>
         </div>
     </div>
@@ -23,7 +25,7 @@
 
     <table class="table table-bordered">
         <tr>
-            
+            <th></th>
             <th>Respuesta</th>
             <th>Es_correcta</th>
             <th>Pregunta_id</th>
@@ -36,28 +38,175 @@
     <tr>
         <td>{{ ++$i }}</td>
         <td>{{ $item->respuesta }}</td>
-        <td>{{ $item->es_correcta }}</td>
+        <td>@if($item->es_correcta)
+                <p>Verdadero</p>
+            @else
+                <p>Falso</p>
+            @endif
+
+            </td>
         <td>{{ $item->pregunta_id }}</td>
         <td>{{ $item->rutaImagen }}</td>
         <td>
-            {{--<a class="btn btn-info" href="{{ route('respuesta.edit',$item->id) }}">Editar</a>--}}
+                        
             <a class="btn btn-info" data-toggle="modal" data-target="#editrespuesta{{ $item->id }}"> 
                 <span class="glyphicon glyphicon-pencil"></span>
             </a>
-
-
             <a class="btn btn-danger" data-toggle="modal" data-target="#erespuesta{{ $item->id }}"> 
                 <span class="glyphicon glyphicon-remove"></span>
             </a>
 
-            
-            
-            
+                        
+            <!-- Modal editar -->
+            <div> 
+                        <div class="modal fade" id="editrespuesta{{ $item->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                          <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <h4 class="modal-title" id="myModalLabel">Editar Pregunta</h4>
+                                </div>
+
+                                <div class="modal-body">
+                                    {!! Form::model($item, ['method' => 'PATCH','route' => ['respuesta.update', $item->id]]) !!}
+                                    <div class="row">
+
+                                        <div class="col-xs-12 col-sm-12 col-md-12">
+                                            <div class="form-group">
+                                                <strong>Respuesta:</strong>
+                                                {!! Form::text('respuesta', null, array('placeholder' => 'respuesta','class' => 'form-control')) !!}
+                                            </div>
+                                        </div>
+
+                                         <div class="form-group">
+                                            <strong>Validez:</strong>
+                                            {!! Form::select('es_correcta', ['' => 'Falso','1' => 'Verdadero'])  !!}
+
+                                        </div>
+
+                                        <div class="col-xs-12 col-sm-12 col-md-12">
+                                            <div class="form-group">
+                                                <strong>Pregunta_id:</strong>
+                                                {!! Form::number('pregunta_id', null, array('placeholder' => 'pregunta','class' => 'form-control')) !!}
+                                            </div>
+                                        </div>
+
+                                        <div class="col-xs-12 col-sm-12 col-md-12">
+                                            <div class="form-group">
+                                                <strong>Ruta:</strong>
+                                                {!! Form::number('rutaImagen', null, array('placeholder' => 'ruta','class' => 'form-control')) !!}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                              <div class="modal-footer">
+                                 <button type="button" class="btn btn-info" data-dismiss="modal">Cancelar</button>
+                                 {!! Form::submit('Guardar', ['class' => 'btn btn-success']) !!}
+                                {!! Form::close() !!}
+                              </div>
+                              
+                            </div>
+                          </div>
+                        </div>
+
+            </div>
+
+
+
+            <!-- Modal eliminar -->
+            <div>
+                <div class="modal fade" id="erespuesta{{ $item->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                  <div class="modal-dialog modal-sm" role="document">
+                    <div class="modal-content">
+
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-title" id="myModalLabel">Confirmar eliminación</h4>
+                        </div>
+                        
+                      <div class="modal-footer">
+                         {!! Form::open(['method' => 'DELETE','route' => ['respuesta.destroy', $item->id],'style'=>'display:inline']) !!}
+                         <button type="button" class="btn btn-success" data-dismiss="modal">Cancelar</button>
+                         {!! Form::submit('Eliminar', ['class' => 'btn btn-danger']) !!}
+                        {!! Form::close() !!}
+                      </div>
+                      
+                    </div>
+                  </div>
+                </div>
+            </div>
+
+           
         </td>
     </tr>
     @endforeach
     </table>
 
-    {!! $items->render() !!}
+
+<script src="https://code.jquery.com/jquery-3.1.1.js" ></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+
+<div>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel">Nueva Respuesta</h4>
+        </div>
+        <div class="modal-body">
+
+            {!! Form::open(array('route' => 'respuesta.store','method'=>'POST')) !!}
+                <div class="row">
+
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Respuesta:</strong>
+                            {!! Form::text('respuesta', null, array('placeholder' => 'respuesta','class' => 'form-control')) !!}
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Es_correcta:</strong>
+                            {!! Form::select('correcta', ['V' => 'Verdadero', 'F' => 'Falso'])  !!}
+                            {{ Form::hidden('es_correcta', '0') }}
+
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Pregunta_id:</strong>
+                            {!! Form::number('pregunta_id', null, array('placeholder' => 'pregunta','class' => 'form-control')) !!}
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Ruta:</strong>
+                            {!! Form::number('rutaImagen', null, array('placeholder' => 'ruta','class' => 'form-control')) !!}
+                        </div>
+                    </div>
+                    
+                </div>
+        </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+        <button type="submit" class="btn btn-primary">Guardar</button>
+      </div>
+      {!! Form::close() !!}
+    </div>
+  </div>
+</div>
+
+<div style="width : 80%; margin : 0 auto;">
+{!! $items->render() !!}
+</div>
 
 @endsection
