c8c18bb96fc97907865bebc4e9b54d3d77d7a4f8
henryxavier10 <hlasso@espol.edu.ec>
2016-10-20 16:43:57 -0500

    slide de taller aun no terminado

--- resources/views/taller/realizar.blade.php
+++ resources/views/taller/realizar.blade.php
@@ -61,6 +61,38 @@
 
         @endforeach
 
+    <!-- Slide-->
+
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+      </ol>
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <embed src="/aulaConocimiento/public/Recursos/Bloque Detener.pdf" type="application/pdf" width="800" height="700"></embed>
+        </div>
+
+        <div class="item">
+           <embed src="/aulaConocimiento/public/Recursos/Bloque Detener.pdf" type="application/pdf" width="800" height="700"></embed>
+        </div>
+
+      </div>
+
+      <!-- Left and right controls -->
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
   
 <script src="https://code.jquery.com/jquery-3.1.1.js" ></script>
 
