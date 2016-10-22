e4525de309c63909abc492353ca8de06d04f6574
rxcampoz <rxcampoz@gmail.com>
2016-10-22 17:41:00 -0500

    Se añade un slide/carrusel para la visualización de las tutorias

--- resources/views/taller/realizar.blade.php
+++ resources/views/taller/realizar.blade.php
@@ -9,79 +9,36 @@
 
    
 
-    <div class="row" style="width : 80%; margin : 0 auto;">
-           
-        <?php $i = '1'; ?>
-<script src="../lightbox/lightbox.min.js"></script>
-        @foreach ($item->recursos as $recurso)
-
-        <div class="row">
-        <strong>Actividad {{$i++}}: </strong>
-        {{ $recurso->descripcion }}<br/>    
-            
-            
-
-        @if($recurso->extension===".mp4")
-         <a class="afterglow btn btn-success" href="#myvideo{{ $recurso->id }}">Ver video </a>
-        <video  id="myvideo{{ $recurso->id }}" width="960" height="540">
-            <source type="video/mp4" src="../Recursos/{{ $recurso->nombre_archivo }}.mp4" />
-        </video>
-        @endif
-
-        @if($recurso->extension===".pdf")
-        <a class="btn btn-success" data-toggle="modal" data-target="#pdf{{ $recurso->id }}"> Ver PDF</a>
-        <!-- Modal -->
-            <div class="modal fade" id="pdf{{ $recurso->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-              <div class="modal-dialog modal-lg" role="document">
-                <div class="modal-content">
-                  <div class="modal-body">
-
-                    <div class="panel panel-inverse">
-                        <div class="panel-heading">
-                            <h4 class="panel-title">{{ $recurso->archivo }}</h4>
-                        </div>
-                        <div class="panel-body">
-                            <iframe src="../ViewerJS/index.html#../Recursos/{{ $recurso->nombre_archivo }}.pdf" width='800' height='600' allowfullscreen webkitallowfullscreen></iframe>
-                        </div>
-                    </div> 
-
-                    
-                   </div>
-                  
-                </div>
-              </div>
-            </div>  
-       
-        @endif
-       
-        @if($recurso->extension===".jpg" || $recurso->extension===".png" || $recurso->extension===".gif")
-        <a class="btn btn-success" href="../Recursos/{{ $recurso->archivo }}" data-lightbox="image{{$recurso->id}}" >Ver Imagen  </a>
-        @endif
-        </div><br/>
-
-        @endforeach
 
     <!-- Slide-->
 
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-      </ol>
-
-      <!-- Wrapper for slides -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false" style="margin: 5%;">
       <div class="carousel-inner" role="listbox">
+        <?php $i = '1'; ?>
+        @foreach ($item->recursos as $recurso)
+        @if($i<2)
         <div class="item active">
-            <embed src="/aulaConocimiento/public/Recursos/Bloque Detener.pdf" type="application/pdf" width="800" height="700"></embed>
-        </div>
-
+        @else
         <div class="item">
-           <embed src="/aulaConocimiento/public/Recursos/Bloque Detener.pdf" type="application/pdf" width="800" height="700"></embed>
+        @endif  
+          <div style="margin-left: 30%; margin-bottom: 2%; margin-top: 3%;">
+            <strong>Actividad {{$i++}}: </strong>
+            {{ $recurso->descripcion }}<br/>  
+          </div>
+          <div style="margin-left: 10%;">
+            @if($recurso->extension===".mp4")
+              <video width="800" height="700" controls><source src="/aulaConocimiento/public/Recursos/{{ $recurso->nombre_archivo }}.mp4" type="video/mp4"></video>
+            @endif
+            @if($recurso->extension===".pdf")
+              <embed src="/aulaConocimiento/public/Recursos/{{ $recurso->nombre_archivo }}.pdf" type="application/pdf" width="800" height="700"></embed>          @endif
+            @if($recurso->extension===".jpg" || $recurso->extension===".png" || $recurso->extension===".gif")
+              <img src="/aulaConocimiento/public/Recursos/{{ $recurso->archivo }}" alt="{{ $recurso->archivo }}" height="800" width="700">        
+            @endif
+          </div>
         </div>
-
+        @endforeach
       </div>
-
+      
       <!-- Left and right controls -->
       <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
@@ -97,11 +54,11 @@
 <script src="https://code.jquery.com/jquery-3.1.1.js" ></script>
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-@if($item->evaluaciones)
-        <a class="btn btn-info" href="{{ URL('realizar_evaluacion',$item->id) }}">Realizar evaluacion  </a>
 
-@endif
-
-    </div>
+<div style="margin-left: 40%;">
+  @if($item->evaluaciones)
+    <a class="btn btn-info" href="{{ URL('realizar_evaluacion',$item->id) }}">Realizar evaluacion  </a>
+  @endif
+</div>
 
 @endsection
